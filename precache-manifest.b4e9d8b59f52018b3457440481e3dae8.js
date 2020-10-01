87428059b65417d4e101349f44d5ca4d20428860
Logan J Vickery <loganjvickery@gmail.com>
2020-10-01 12:48:21 -0700

    Refactoring and using React
    Completely redid the code and moved to React.

---
+++ precache-manifest.b4e9d8b59f52018b3457440481e3dae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2972521f346f2d21d56ac9e3dbe1837f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3bde30428fde0cbbb18d",
+    "url": "./static/css/main.beebfed4.chunk.css"
+  },
+  {
+    "revision": "79f9be8775f823ba2048",
+    "url": "./static/js/2.dea857ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.dea857ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bde30428fde0cbbb18d",
+    "url": "./static/js/main.70fa2d88.chunk.js"
+  },
+  {
+    "revision": "df86eed40c622f62d0a5",
+    "url": "./static/js/runtime-main.47b0b79b.js"
+  }
+]);
