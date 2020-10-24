65e5821daab29cf2458db1b1bd50deaf77bc5225
Logan <loganjvickery@gmail.com>
2020-10-23 17:28:04 -0700

    refactor complete!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-/* App not working as written below.  Refactor in progress as of OCt.21.2020*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './planets.css';
@@ -14,28 +12,33 @@ import Saturn from "./saturn.png";
 import Uranus from "./uranus.png";
 import Neptune from "./neptune.png";
 
-var today = new Date();
+const today = new Date();
 const months = ["January","February","March","April","May","June","July",
 "August","September","October","November","December"];
-const astroList = ['Neptune','Uranus','Saturn','Jupiter','Mars','Earth','Venus','Mecury','Sun'];
+const astroList = ['Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune'];
 // The length of a year in Earth days of each planet in order.  0 = mercury, 1 = venus, etc
 const orbitDays = [87.969, 224.7, 365.2564, 686.98, 4332.82, 10755.7, 30687.15, 60190.03];
 // Radial offsets planets bases on Jan 1 2016 position
 const originOffset = [351.4, 201.17, 286.18, 221.48, 229.42, 142.71, 12.62, 51.97];
+const daysInMonth = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.updateDays = this.updateDays.bind(this);
+    this.updatePlanets = this.updatePlanets.bind(this);
     this.state = {
       date: today.getDate(),
       month: today.getMonth(),
       year: today.getFullYear(),
       totalDays: 31,
+      planetOffsets:[0,0,0,0,0,0,0,0],
     };
   }
+  componentDidMount() {
+    this.updatePlanets();
+  }
   updateDays(event) {
-    console.log(event.target.value);
     let month = event.target.value;
     let totalDays = 31;
     switch(month) {
@@ -52,22 +55,51 @@ class App extends React.Component {
     let newState = this.state;
     newState.month = month;
     newState.totalDays = totalDays;
-    console.log(totalDays)
-    this.setState(newState);
-    console.log(this.state.month)
   }
   updatePlanets(event) {
-    console.log('do a little dance');
-    // will add more code here later
+    // update state on change
+    let newState = this.state;
+    if (event) {
+      let handler = event.target;
+      switch(handler.id) {
+        case "DayDrop":
+          newState.date =  handler.value;
+          break;
+        case "MonthDrop":
+          newState.month =  handler.value;
+          break;
+        case "YearSel":
+          newState.year =  handler.value;
+          break;
+        default:
+      }
+    }
 
+    // find days since 1 Jan, 2016
+    let daysSince = Number(newState.date) + Number(daysInMonth[newState.month]);
+    // add a day for leap years
+    if (newState.year % 4 == 0 && newState.month > 1) {
+      daysSince += 1;
+    }
+    daysSince += (newState.year - 2016) * 365.2564;
+    console.log(newState.date + " day, " + newState.month + " month, " + newState.year + " year")
+    // calculates the degree of rotation base on orbit speed and date
+    function calcRotate(daysInYear, daysR, mod) {
+      return (((daysR % daysInYear) / daysInYear) * -360) + mod;
+    }
+    // changes the position of the planets
+    for (let i = 0; i < astroList.length; i++) {
+      newState.planetOffsets[i] = calcRotate(orbitDays[i], daysSince, originOffset[i]);
+    }
+    this.setState(newState);
   }
+  
   render() {
     return (
       <div className="app">
         <header className="contain">
-          <h2>Where Are the Planets? </h2>
+          <h2>Where Are the Planets? </h2>          
           <nav id="change" onChange={this.updatePlanets}>
-            {console.log(this.state)}
 
             <DayDrop defaultValue={this.state.date} totalDays={this.state.totalDays} />
             <MonthDrop defaultValue={this.state.month} onChange={this.updateDays} />
@@ -78,7 +110,7 @@ class App extends React.Component {
         </header>
 
         <main>
-          <Planets />
+          <Planets offsets={this.state.planetOffsets} />
         </main>
 
         <footer className="contain">
@@ -90,15 +122,6 @@ class App extends React.Component {
 }
 
 class DayDrop extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     totalDays: 30,
-  //   };
-  // }
-  // onChange(value,field) {
-  //   this.setState({value: value});
-  // }
   render() {
     return (
       <select name="day" id="DayDrop" defaultValue={this.props.defaultValue}>
@@ -109,16 +132,10 @@ class DayDrop extends React.Component {
 }
 
 class MonthDrop extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      totalDays: 31,
-    };
-  }
   render() {
     return (
       <select name="month" id="MonthDrop" defaultValue={this.props.defaultValue} onChange={this.props.onChange}>
-        {Array.from(Array(months.length)).map((x, index) => <option key={index} value={index}>{months[index]}</option>)}
+        {Array.from(Array(months.length)).map((x, index) => <option key={months[index]} value={index}>{months[index]}</option>)}
       </select>
     )
   }
@@ -133,23 +150,17 @@ class YearSel extends React.Component {
 }
 
 class Planets extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      };
-  }
   render() {
     return (
       <div id="planets">
-        {/* {Array.from(Array(astroList.length)).map((x, index) => <img key={index} id={astroList[index]} src={astroList[index]} alt={astroList[index]} />)} */}
-        <img id="Neptune" src={Neptune} alt="neptune" />
-        <img id="Uranus" src={Uranus} alt="uranus" />
-        <img id="Saturn" src={Saturn} alt="saturn" />
-        <img id="Jupiter" src={Jupiter} alt="jupiter" />
-        <img id="Mars" src={Mars} alt="mars" />
-        <img id="Earth" src={Earth} alt="earth" />
-        <img id="Venus" src={Venus} alt="venus" />
-        <img id="Mercury" src={Mercury} alt="mercury" />
+        <img id="Neptune" src={Neptune} alt="neptune" style={{transform: "rotate(" + this.props.offsets[7] + "deg)"}} />
+        <img id="Uranus" src={Uranus} alt="uranus" style={{transform: "rotate(" + this.props.offsets[6] + "deg)"}} />
+        <img id="Saturn" src={Saturn} alt="saturn" style={{transform: "rotate(" + this.props.offsets[5] + "deg)"}} />
+        <img id="Jupiter" src={Jupiter} alt="jupiter" style={{transform: "rotate(" + this.props.offsets[4] + "deg)"}} />
+        <img id="Mars" src={Mars} alt="mars" style={{transform: "rotate(" + this.props.offsets[3] + "deg)"}} />
+        <img id="Earth" src={Earth} alt="earth" style={{transform: "rotate(" + this.props.offsets[2] + "deg)"}} />
+        <img id="Venus" src={Venus} alt="venus" style={{transform: "rotate(" + this.props.offsets[1] + "deg)"}} />
+        <img id="Mercury" src={Mercury} alt="mercury" style={{transform: "rotate(" + this.props.offsets[0] + "deg)"}} />
         <img id="Sun" src={Sun} alt="sun" />
       </div>
     )
@@ -162,59 +173,4 @@ ReactDOM.render(
 );
 
 
-
-// //Returns the # of days since 1 Jan, 2016
-// function calcDaysSince(newDate) {
-//   let days = newDate.getDate();
-//   let thisMonth = newDate.getMonth();
-//   let daysInMonth = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
-//   days += daysInMonth[newDate.getMonth()];
-//   // This function adds a day if it's a leap year
-//   if (thisMonth > 1) {
-//     if (newDate.getFullYear() % 4 === 0) {
-//       days += 1;
-//     }
-//   }
-//   days += (newDate.getFullYear() - 2016) * 365.2564;
-//   return days;
-// }
-
-// //Calculates the degree of rotation when you pass in the length of one year of a planet and the number of days since
-// function calcRotate(daysInYear, daysR, mod) {
-//   return (((daysR % daysInYear) / daysInYear) * -360) + mod;
-// }
-
-// function moveTheThings() {
-//   // document.querySelectorAll('#DayDrop option')[28].removeAttribute('disabled');
-//   // document.querySelectorAll('#DayDrop option')[29].removeAttribute('disabled');
-//   // document.querySelectorAll('#DayDrop option')[30].removeAttribute('disabled');
-//   let month = document.querySelector('#MonthDrop').value;
-//   if (month === "1") {
-//     if (document.querySelector('#DayDrop').value > "28") {
-//       document.querySelector('#DayDrop').value = "28";
-//     }
-//     // document.querySelectorAll('#DayDrop option')[28].disabled = "true";
-//     // document.querySelectorAll('#DayDrop option')[29].disabled = "true";
-//     // document.querySelectorAll('#DayDrop option')[30].disabled = "true";
-//   } else if (month === "3" || month === "5" || month === "8" || month === "10") {
-//     if (document.querySelector('#DayDrop').value > "30") {
-//       document.querySelector('#DayDrop').value = "30";
-//     }
-//     // document.querySelectorAll('#DayDrop option')[30].disabled = "true";
-//   }
-
-//   // Grabs the date info from from the inputs
-//   today.setDate(document.querySelector('#DayDrop').value);
-//   today.setMonth(document.querySelector('#MonthDrop').value);
-//   today.setYear(document.querySelector('#YearSel').value);
-//   let daysSince = calcDaysSince(today);
-//   //Changes the position of the planets
-//   for (let i = 0; i < astroList.length; i++) {
-//     document.querySelector('#' + astroList[i]).style.transform = 'rotate(' + calcRotate(orbitDays[i], daysSince, originOffset[i]) + 'deg)';
-//   }
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   moveTheThings();
-// });
 
